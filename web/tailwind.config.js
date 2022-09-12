e883357eb144044f1374176e4519705cba24035a
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-09-12 19:45:26 -0300

    web - update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["inter var", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        galaxy: "url('images/background.jpg')",
+        "nlw-radiant":
+          "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);",
+      },
+    },
   },
   plugins: [],
 };
