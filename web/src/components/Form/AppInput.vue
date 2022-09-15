a4f60dc319a20a2ab4aac9e19b0839642f1c2d33
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-09-14 21:49:16 -0300

    web - fix input emit

--- web/src/components/Form/AppInput.vue
+++ web/src/components/Form/AppInput.vue
@@ -5,7 +5,9 @@ defineEmits(["update:modelValue"]);
 <template>
   <input
     v-bind="$attrs"
-    @input="$emit('update:modelValue', $event.target.value)"
+    @input="
+      $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+    "
     class="p-4 text-sm text-white rounded-lg bg-zinc-900 placeholder:text-zinc-500 focus:border-violet-500 focus:ring-violet-500"
   />
 </template>
