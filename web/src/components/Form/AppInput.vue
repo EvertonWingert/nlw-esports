b6b6eb36ca322fb06fdff4bd53a70289f1cd4705
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-09-14 21:48:04 -0300

    Update AppInput.vue

--- web/src/components/Form/AppInput.vue
+++ web/src/components/Form/AppInput.vue
@@ -5,7 +5,7 @@ defineEmits(["update:modelValue"]);
 <template>
   <input
     v-bind="$attrs"
-    @input="$emit('update:modelValue', $event.target.value)"
+    @input="$emit('update:modelValue', $event.target?.value)"
     class="p-4 text-sm text-white rounded-lg bg-zinc-900 placeholder:text-zinc-500 focus:border-violet-500 focus:ring-violet-500"
   />
 </template>
